992198517865b361acbfefb49b4c60b32cc9cf50
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-24 19:44:53 +0000

    begin other functions

--- test.js
+++ test.js
@@ -24,3 +24,18 @@ test("One euro should be 1.07 dollars", function() {
     // This is the comparison for the unit test
      expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro is 1.07 dollars, then 3.5 euros should be = (3.5 * 1.07)
 })
+
+
+// Third Test - USD => JPY (Dollar To Yen)
+test("One Dollar should be 142.535 Yen", function(){
+    const { fromDollarToYen} = require('./app.js');
+
+    // Use the function
+    const dollars = fromDollarToYen(5);
+
+    // IF 1 dollar is 142.535 Yen, then 5 dollars should be 5 * 142.535 = 712.675
+})
+
+
+// Fourth Test - JPY => GBP (Yen to Pound)
+test("One Yen should be x Pounds")
