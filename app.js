74eeb436629f7e1846d0fa3e232dbf0e0c14d2bb
Dwane Dunn <dwane.dunn@gmail.com>
2025-04-24 19:13:50 +0000

    add second function

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
 // This is my function that sums two numbers
 const sum = (a,b) => {
     return a + b
@@ -8,4 +16,4 @@ console.log(sum(7,3))
 
 // Export the function to be used on other files 
 // (similar to the keyword "export" when using webpack)
-module.exports = { sum };
+module.exports = { sum,fromEuroToDollar };
